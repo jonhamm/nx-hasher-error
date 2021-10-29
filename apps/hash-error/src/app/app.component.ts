58442045a13610e6dd2d68a563cf612f3a4e521c
Jon Hammerskov <jon@hammerskov.dk>
2021-10-29 08:16:28 +0200

    initial nx workspace with one angular app

---
+++ apps/hash-error/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-hash-error-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'hash-error';
+}
